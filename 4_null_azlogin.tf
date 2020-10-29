b768a7f1b6f3832199a60282f450052123c8212e
Sai Sasank Anaparthi <47329009+saisasank@users.noreply.github.com>
2020-10-29 10:51:13 -0500

    Update 4_null_azlogin.tf

--- 4_null_azlogin.tf
+++ 4_null_azlogin.tf
@@ -1,6 +1,10 @@
 #+=======================================================================================
 # Login to Azure
 resource "null_resource" "az_login" {
+  triggers = {
+    version = local.file_version_name
+  }
+  
   provisioner "local-exec" {
     command = "az login --service-principal -u ${var.deployment.client_id} -p ${var.deployment.client_secret} --tenant ${var.deployment.tenant_id}"
   }
@@ -11,6 +15,10 @@ resource "null_resource" "az_login" {
 #+=======================================================================================
 # Set Azure Subscription
 resource "null_resource" "az_subscription_set" {
+  triggers = {
+    version = local.file_version_name
+  }
+  
   provisioner "local-exec" {
     command = "az account set --subscription ${var.deployment.subscription_id}"
   }
