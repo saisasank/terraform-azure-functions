d517deb13672eab2ea450a47c98b0fe374534d93
Sai Sasank Anaparthi <47329009+saisasank@users.noreply.github.com>
2021-01-08 16:25:15 -0600

    Update 4_null_download.tf

--- 4_null_download.tf
+++ 4_null_download.tf
@@ -5,7 +5,7 @@ resource "null_resource" "download" {
     version = local.file_version_name
   }
   provisioner "local-exec" {
-    command = "az storage blob download --file ./${local.file_version_name} --name ${local.file_version_name} --subscription ${var.artifacts.subscription} --container-name ${var.artifacts.container_name} --account-name ${var.artifacts.storage_account} --account-key UqV/oMMBlPaheBi3eDJeEEx14R/EZ8jGuVULCeRqMvbxrVAqGoLZ/OKe03rKBAwjYWk28Q7BHXfA/zQLd5g0mg=="
+    command = "az storage blob download --file ./${local.file_version_name} --name ${local.file_version_name} --subscription ${var.artifacts.subscription} --container-name ${var.artifacts.container_name} --account-name ${var.artifacts.storage_account} --account-key ${var.artifacts.account_key}"
   }
  
   depends_on = [
